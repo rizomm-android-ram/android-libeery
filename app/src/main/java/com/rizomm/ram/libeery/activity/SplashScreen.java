4174f0e432a195281721b5906feec5c2aed9e789
Amoriz <amaury.trubert@laposte.net>
2015-04-26 14:19:57 +0200

    #2 splashscreen
    Mise en place de la structure du splashScreen.
    Reste la partie graphique de cette vue à faire.
    Définition de l'activité SplashScreen comme activité de démarrage de
    l'application.

---
+++ app/src/main/java/com/rizomm/ram/libeery/activity/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.rizomm.ram.libeery.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.os.Handler;
+
+import com.rizomm.ram.libeery.MainActivity;
+import com.rizomm.ram.libeery.R;
+
+/**
+ * Classe permettant de gérer un splashScreen.
+ * Définie comme activité de lancement dans le Manifest.
+ */
+public class SplashScreen extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
